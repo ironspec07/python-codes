382fca47c5793aec8e60fcc65e4a4fa8f4a9efd9
ironspec07 <ironspec07@gmail.com>
2023-11-25 18:44:17 +0530

    Day-2

--- Lectures/L0_functions.py
+++ Lectures/L0_functions.py
@@ -32,3 +32,71 @@ print(f"hello,{name}")# printing using format string
 print(f"hello,{first}")# printing firstname 
 print(f"hello,{last}")# printing lastname 
 
+#######TYPECASTING###################################################################################
+num = input("enter number ") #taking input
+
+num = int(num) #typecasting string to int
+
+num = str(num) #typecasting int to str
+
+num = num+num
+print(num)
+
+num = num*7
+print(num)
+
+# round function to convert from float to int
+# round(number [,ndigits])
+
+# number --> value to be converted (takes only one value)
+# ndigits --> number of places to be rounded to [optional]
+
+x = float(input("enter number 1: "))
+y = float(input("enter number 2: "))
+
+z = round(x+y)
+w = x/y
+
+print(f"{z:,}")#prints sum in currency style --> 1,000(us convention)
+print(f"{w:.2f}")#prints two digits after decimal
+
+
+#######DEFINING FUNCTION###########################################################################
+
+def hello():
+    print("hello,",end="")
+
+name = input("what's your name? ")
+hello()
+print(name)
+
+def hello2(to): #parameterizing hello
+    print("hello,", to)
+
+hello2(name)
+
+def hello2(to="world"):#inialising hello with default value if user does not provide parameter
+    print("hello,", to)
+
+hello2()
+#######################################################################################################
+def main():#defining main function 
+    name = input("what's your name? ")
+    hello2(name)
+
+def hello2(to): 
+    print("hello,", to)
+
+main() #calling main function
+
+#scope of main is only in main function
+####################################RETURNING########################################################
+def main():#defining main function 
+    num = int(input("what's num? "))
+    print("square of number is " , square(num))
+
+def square(n): 
+    #return n*n #returning value
+    #return n**2 #raising to the power of two
+    return pow(n,2)#raising to the power of two
+main() #calling main function
---
+++ practice/gst.py
@@ -0,0 +1,18 @@
+cost = int(input("enter total cost: "))
+
+cgst = 0.09*cost
+print(f"Add CGST 09% {cgst}")
+sgst = 0.09*cost
+print(f"Add SGST 09% {sgst}")
+
+print(f"total cost of product:{cost+sgst+cgst}")
+
+
+from tabulate import tabulate
+mydata = [
+    ["Add: CGST 09% ", cgst], 
+    ["Add: SGST 09% ", sgst], 
+    ["total cost of product:",(cost+sgst+cgst) ], 
+]
+head = ["Particulars", "GST on particulars"]
+print(tabulate(mydata, headers=head, tablefmt="grid"))
