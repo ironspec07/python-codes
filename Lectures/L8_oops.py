cdb3e5de3e29b763a212cf3f54d4d3aee13efa0e
ironspec07 <ironspec07@gmail.com>
2024-01-07 15:57:25 +0530

    Day 17: OOPs

--- Lectures/L8_oops.py
+++ Lectures/L8_oops.py
@@ -1,80 +1,80 @@
-# # Represents a student with multiple variables
+# Represents a student with multiple variables
 
-# name = input("Name: ")
-# house = input("House: ")
-# print(f"{name} from {house}")
-
-
-# # Modularizes getting student's name and house
-
-# def main():
-#     name = get_name()
-#     house = get_house()
-#     print(f"{name} from {house}")
+name = input("Name: ")
+house = input("House: ")
+print(f"{name} from {house}")
 
 
-# def get_name():
-#     return input("Name: ")
+# Modularizes getting student's name and house
+
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
 
 
-# def get_house():
-#     return input("House: ")
+def get_name():
+    return input("Name: ")
 
 
-# if __name__ == "__main__":
-#     main()
-
-# # Returns student as tuple, unpacking it
+def get_house():
+    return input("House: ")
 
 
-# def main():
-#     name, house = get_student()
-#     print(f"{name} from {house}")
+if __name__ == "__main__":
+    main()
+
+# Returns student as tuple, unpacking it
 
 
-# def get_student():
-#     name = input("Name: ")
-#     house = input("House: ")
-#     return name, house 
+def main():
+    name, house = get_student()
+    print(f"{name} from {house}")
 
 
-# if __name__ == "__main__":
-#     main()
-
-# # Returns student as tuple, without unpacking it
-
-
-# def main():
-#     student = get_student()
-#     print(f"{student[0]} from {student[1]}")
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house 
 
 
-# def get_student():
-#     name = input("Name: ")
-#     house = input("House: ")
-#     return (name, house)
+if __name__ == "__main__":
+    main()
+
+# Returns student as tuple, without unpacking it
 
 
-# if __name__ == "__main__":
-#     main()
-
-# # Demonstrates immutability of tuples, removes parentheses
-
-# def main():
-#     student = get_student()
-#     if student[0] == "Padma":
-#         student[1] = "Ravenclaw"
-#     print(f"{student[0]} from {student[1]}")
+def main():
+    student = get_student()
+    print(f"{student[0]} from {student[1]}")
 
 
-# def get_student():
-#     name = input("Name: ")
-#     house = input("House: ")
-#     return name, house
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return (name, house)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
+
+# Demonstrates immutability of tuples, removes parentheses
+
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
+
+
+if __name__ == "__main__":
+    main()
 
 ## Stores student as (mutable) list
 
@@ -90,6 +90,368 @@ def get_student():
     name = input("Name: ")
     house = input("House: ")
     return [name, house]
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Stores student as dict
+
+def main():
+    student = get_student()
+    print(f"{student['name']} from {student['house']}")
+
+
+def get_student():
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Eliminates unneeded variable
+
+def main():
+    student = get_student()
+    print(f"{student['name']} from {student['house']}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# Demonstrates mutability of dicts
+
+def main():
+    student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
+
+if __name__ == "__main__":
+    main()
+
+##########################################################################################
+
+# Defines class for a student
+class Student:
+    ...
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    student = Student() # creating a object of student class.
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+# Adds __init__
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
+    return student
+
+
+if __name__ == "__main__":
+    main()
+    
+# Eliminates unneeded variable
+
+
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+    
+# Adds validation in __init__ using raise
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+
+# Adds __str__ to print 
+
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+def main():
+    student = get_student()
+    print(student) 
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+
+# Prompts for patronus too, but doesn't display yet
+class Student:
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+
+def main():
+    student = get_student()
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
+
+
+if __name__ == "__main__":
+    main()
+
+# Adds charm method to cast a charm
+class Student:
+    def __init__(self, name, house, patronus=None):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        if patronus and patronus not in ["Stag", "Otter", "Jack Russell terrier"]:
+            raise ValueError("Invalid patronus")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐎"
+            case "Otter":
+                return "🦦 "
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "🪄"
+
+
+def main():
+    student = get_student()
+    print("Expecto Patronum!")
+    print(student.charm())
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ") or None
+    return Student(name, house, patronus)
+
+
+if __name__ == "__main__":
+    main()
+
+# Removes patronus for simplicy, circumvents error-checking by setting attribute
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Invalid name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+
+def main():
+    student = get_student()
+    student.house = "Number Four, Privet Drive"
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Adds @property for house
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Invalid name")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @property #getter
+    def house(self):
+        return self._house
+
+    @house.setter #setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
+def main():
+    student = get_student()
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+
+# Adds @property for name
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Invalid name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
+def main():
+    student = get_student()
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 
 if __name__ == "__main__":
